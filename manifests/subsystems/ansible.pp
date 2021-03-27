c919939e48e0f313855d05200d0fb79657015cd1
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-03-27 20:32:38 +0000

    Commit

--- manifests/subsystems/ansible.pp
+++ manifests/subsystems/ansible.pp
@@ -6,7 +6,7 @@ class vs_devops::subsystems::ansible (
         ensure  => 'present',
     }
     
-    if ( $ansibleConfig['galaxyRolesUpdate'] == true )
+    if ( $ansibleConfig['rolesUpdate'] == true )
     {
         $ansibleConfig['galaxyRoles'].each |String $role|
         {
@@ -14,6 +14,24 @@ class vs_devops::subsystems::ansible (
                 command => "/usr/bin/ansible-galaxy install ${role} -p ${ansibleConfig['pathRoles']} --ignore-errors",
                 require => Class['ansible'],
                 onlyif  => '/usr/bin/test -e /usr/bin/ansible-galaxy',
+            }
+        }
+        
+        $ansibleConfig['ansibleRoles'].each |String $role, String $url|
+        {
+            $file_path      = "${ansibleConfig['pathRoles']}/${role}"
+            $file_exists    = find_file( $file_path )
+            if ( $file_exists ) {
+                exec{ "Fetch Role ${role}":
+                    command => "/usr/bin/git pull",
+                    cwd     => "${ansibleConfig['pathRoles']}/${role}",
+                    user    => 'vagrant',
+                }
+            } else {
+                exec{ "Fetch Role ${role}":
+                    command => "/usr/bin/git clone ${url} ${ansibleConfig['pathRoles']}/${role}",
+                    user    => 'vagrant',
+                }
             }
         }
     }
