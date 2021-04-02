fa6179e40f6cb83ff88dcd3737b1e99c1facb747
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-04-02 10:20:02 +0000

    Adding Logstash with config.

--- manifests/subsystems/elastic_stack.pp
+++ manifests/subsystems/elastic_stack.pp
@@ -1,24 +1,20 @@
 class vs_devops::subsystems::elastic_stack (
     Hash $config    = {},
 ) {
-
     stage { 'elastic_stack_late_install': }
     Stage['main'] -> Stage['elastic_stack_late_install']
+    
     class { 'elasticsearch':
         version     => '7.12.0',
         
-        api_host    => 'devops.lh',
+        api_host    => 'localhost',
         api_port    => 9200,
         config      => {
             'cluster'   => {
-                'name'                  => 'ClusterName',
+                'name'                  => 'VsElkCluster',
                 'initial_master_nodes'  => ["devops.lh"]
             }
         }
-    }
-    
-    class { 'logstash':
-        stage   => 'elastic_stack_late_install',
     }
     
     class { 'kibana':
@@ -27,15 +23,19 @@ class vs_devops::subsystems::elastic_stack (
             'server.host' => '10.3.3.3',
             'server.name' => 'devops.lh',
             
-            'elasticsearch.hosts'           => ["http://localhost:9200"]
+            'elasticsearch.hosts'           => ["http://localhost:9200"],
             'elasticsearch.requestTimeout'  => '180000',
         },
     }
     
-    # You must provide a valid pipeline configuration for the service to start.
-    /*
-    logstash::configfile { 'my_ls_config':
-        content => template('path/to/config.file'),
+    class { 'logstash':
+        stage   => 'elastic_stack_late_install',
     }
-    */
+    
+    # You must provide a valid pipeline configuration for the service to start.
+    logstash::configfile { 'logstash_config':
+        content => template( "${module_name}/elk/logstash/logstash.conf.erb" ),
+        stage   => 'elastic_stack_late_install',
+        require => Class['logstash'],
+    }
 }
