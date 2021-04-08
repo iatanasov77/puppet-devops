8702e82685e1da445fa1f5d8b7680861083016ce
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-04-08 12:56:25 +0000

    Commit

--- manifests/subsystems/hashicorp/vaultEnvironment.pp
+++ manifests/subsystems/hashicorp/vaultEnvironment.pp
@@ -1,8 +1,8 @@
 class vs_devops::subsystems::hashicorp::vaultEnvironment (
     String $vaultPort   = '8200',
 ) {
-    $vaultKeys  = $vault_keys
-    $vaultToken = $vaultKeys['root_token']
+    $vaultToken = $facts.get( 'vault_keys.root_token', 'UNDEFINED' )
+    $unsealKey = $facts.get( 'vault_keys.keys_base64.0', 'UNDEFINED' )
     File { "vault_profile.sh":
         ensure  => file,
         path    => "/etc/profile.d/vault.sh",
@@ -12,7 +12,7 @@ class vs_devops::subsystems::hashicorp::vaultEnvironment (
     
     Exec { 'Unseal Vault':
         environment => ["VAULT_TOKEN='${vaultToken}'"],
-        command => "curl --request POST --data '{\"key\": \"${vaultKeys['keys_base64'][0]}\"}' http://127.0.0.1:${vaultPort}/v1/sys/unseal > /tmp/vault_unseal.json",
+        command => "curl --request POST --data '{\"key\": \"${unsealKey}\"}' http://127.0.0.1:${vaultPort}/v1/sys/unseal > /tmp/vault_unseal.json",
         user    => 'vagrant',
     }
 }
