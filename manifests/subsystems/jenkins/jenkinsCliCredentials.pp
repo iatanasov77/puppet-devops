26b1c46b7ea90d50c8478187760373333f05c37c
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-04-10 10:28:20 +0000

    Commit

--- manifests/subsystems/jenkins/jenkinsCliCredentials.pp
+++ manifests/subsystems/jenkins/jenkinsCliCredentials.pp
@@ -10,11 +10,8 @@ class vs_devops::subsystems::jenkins::jenkinsCliCredentials (
         vs_devops::subsystems::jenkins::credentialXMl { "jenkins-credential-${id}":
             crdId   => $id,
             config  => $crd,
-        } ->
-        
-        notify { "JENKINS CREDENTIALS TYPE: '${crd['type']}'":
-            withpath => false,
         }
+        
         if ( $crd['type'] == 'SSHUserPrivateKey' and $readPrivateKeys ) {
             Exec { "Set PrivateKey for: ${id}":
                 command => "${readPrivateKeys} -i${id}",
