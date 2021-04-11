31a9ee906a4758d2d0739630dcc6f2af41a90c26
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-04-11 14:30:33 +0000

    Commit

--- manifests/subsystems/jenkins/jobXml.pp
+++ manifests/subsystems/jenkins/jobXml.pp
@@ -2,15 +2,19 @@ define vs_devops::subsystems::jenkins::jobXml (
     String $jobId,
     Hash $config,
 ) {
-    if $config['type']  == 'Pipeline' {
-        $fetchPipelineCommand   = "/usr/bin/cat ${config['pipeline']}"
-        notify { "FETCH PIPELINE COMMAND ${jobId}: ${fetchPipelineCommand}":
-            withpath => false,
+    if ( $config['createXml'] ) {
+        $xmlConfig  = to_json( $config['config'] )
+        Exec { "Create Job Xml /tmp/jenkins-job-${jobId}.xml":
+            command => "${config['createXml']} -t '${config['type']}' -i '${jobId}' -c '${xmlConfig}'"
         }
-    }
+    } else {
+        if $config['type']  == 'Pipeline' {
+            $fetchPipelineCommand   = "/usr/bin/cat ${config['pipeline']}"
+        }
     
-    File { "/tmp/jenkins-job-${jobId}.xml":
-        ensure  => file,
-        content => template( "vs_devops/jenkins/jobs/${config['type']}.xml.erb" ),
+        File { "/tmp/jenkins-job-${jobId}.xml":
+            ensure  => file,
+            content => template( "vs_devops/jenkins/jobs/${config['type']}.xml.erb" ),
+        }
     }
 }
