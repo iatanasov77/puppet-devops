e373b42b6e6548f1e9dd6681ef0bcef1cd731f31
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-05-30 12:33:50 +0000

    Update Docker Configuration.

--- manifests/subsystems/docker.pp
+++ manifests/subsystems/docker.pp
@@ -6,4 +6,9 @@ class vs_devops::subsystems::docker (
         version         => 'latest',
         docker_users    => $config['docker_users'],
     }
+    
+    class {'docker::compose':
+        ensure => present,
+        #version => '1.9.0',
+    }
 }
